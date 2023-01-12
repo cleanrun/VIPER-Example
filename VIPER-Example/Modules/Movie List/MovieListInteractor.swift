c852ee9c512a38c073b30aaf2ecf1dbf1585e7aa
cleanrun <marchellehcram@gmail.com>
2023-01-12 12:42:54 +0700

    update key

--- VIPER-Example/Modules/Movie List/MovieListInteractor.swift
+++ VIPER-Example/Modules/Movie List/MovieListInteractor.swift
@@ -12,7 +12,7 @@ final class MovieListInteractor: MovieListPresenterToInteractorProtocol {
     weak var presenter: MovieListInteractorToPresenterProtocol?
     
     func fetchMovieList() {
-        let urlString = "https://api.themoviedb.org/3/movie/now_playing?api_key=e9325a84053f572e130d8028237ab315&language=en-US&page=1"
+        let urlString = "https://api.themoviedb.org/3/movie/now_playing?api_key=&language=en-US&page=1"
         Task {
             do {
                 let response = try await webservice.request(endpoint: urlString, type: MovieListEntity.self)
